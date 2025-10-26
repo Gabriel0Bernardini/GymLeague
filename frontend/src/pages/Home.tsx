f3151726a7c246295d12c4724582e9811b5293d1
Lucas Daniel <lucasdmaciel12@gmail.com>
2025-10-26 19:59:21 -0300

    Kan-105: Criação da tela do perfil

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,17 @@
+
+import { FaUser } from "react-icons/fa";
+
+
+export default function Home() {
+  return (
+    <div>
+      <div className="p-4 bg-blue-600 text-white flex justify-end">
+        <button ><FaUser className="h-6 w-6"/></button>
+      </div>
+      <div className="flex items-center justify-center bg-gray-100">
+        <h1>Bem-vindo ao GymLeague</h1>
+      </div>
+    </div>
+
+  );
+}
