02606daf9cc78d29c1fd99f346d6ae01c56d3146
girafinos <felipeprinci@gmail.com>
2025-12-03 16:19:31 -0300

    KAN"definir": primeiros testes da implementação do crude da tela de  "iniciar treino".

--- backend/app.py
+++ backend/app.py
@@ -11,6 +11,7 @@ from routes.grupos_routes import grupos_bp
 from routes.musculos_routes import musculos_bp
 from routes.inserirExercicio import inserirExercicios_bp
 from routes.explorarRotinas import explorarRotinas_bp
+from routes.treinar_routes import treinar_bp
 
 load_dotenv()
 
@@ -28,6 +29,7 @@ app.register_blueprint(grupos_bp)
 app.register_blueprint(musculos_bp)
 app.register_blueprint(inserirExercicios_bp)
 app.register_blueprint(explorarRotinas_bp)
+app.register_blueprint(treinar_bp)
 
 @app.get("/health")
 def health():
