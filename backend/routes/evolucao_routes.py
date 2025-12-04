75dfcb9672eac89aa1d547f72f84f0fd9a818f81
Gabriel0Bernardini <gabrielbr1809@gmail.com>
2025-12-04 16:06:13 -0300

    Mandnado arquivo com imports

--- backend/routes/evolucao_routes.py
+++ backend/routes/evolucao_routes.py
@@ -1,7 +1,13 @@
 from db import get_conn
-from flask import Blueprint, jsonify
+from utils.auth import require_auth
+from flask import Blueprint, jsonify, request, g
 
 evolucao_bp = Blueprint("evolucao", __name__, url_prefix="/evolucao")
 
-@evolucao_bp.get("/")
-def get_treinoCompleto
+@evolucao_bp.get("/treinoCompletoData")
+@require_auth
+def get_treinoCompletoData():
+    email = g.user['email']
+    conn = get_conn()
+    cursor = conn.cursor(dictionary=True)   
+    
